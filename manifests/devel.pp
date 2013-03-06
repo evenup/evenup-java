e5b87f9a4e8db99f9da3339931c404c542be55b8
Justin Lambert <jlambert@eml.cc>
2013-03-05 20:34:49 -0700

    add ensure parameter to classes

--- manifests/devel.pp
+++ manifests/devel.pp
@@ -1,8 +1,38 @@
-# TODO - document me
-class java::devel {
+# == Class: java::devel
+#
+# This class installs the java development package
+#
+# === Parameters
+#
+# [*ensure*]
+#   String.  Controls whether java-devel should be installed or absent.
+#   Valid options are: present, absent, latest, installed, or a specific version
+#
+#
+# === Examples
+#
+# * Installation:
+#     class { 'java::devel': }
+#
+# * Removal/decommissioning:
+#     class { 'java::devel':
+#       ensure => 'absent',
+#     }
+#
+# === Authors
+#
+# * Justin Lambert <mailto:jlambert@letsevenup.com>
+#
+# === Copyright
+#
+# Copyright 2013 EvenUp.
+#
+class java::devel (
+  $ensure => 'latest'
+){
 
   package {
     'java-1.6.0-openjdk-devel.x86_64':
-      ensure  => installed;
+      ensure  => $ensure,
   }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,38 @@
-#TODO document me
-class java {
+# == Class: java
+#
+# This class installs the java package
+#
+# === Parameters
+#
+# [*ensure*]
+#   String.  Controls whether java should be installed or absent.
+#   Valid options are: present, absent, latest, installed, or a specific version
+#
+#
+# === Examples
+#
+# * Installation:
+#     class { 'java': }
+#
+# * Removal/decommissioning:
+#     class { 'java':
+#       ensure => 'absent',
+#     }
+#
+# === Authors
+#
+# * Justin Lambert <mailto:jlambert@letsevenup.com>
+#
+# === Copyright
+#
+# Copyright 2013 EvenUp.
+#
+class java (
+  $ensure => 'latest'
+){
 
   package { 'java-1.6.0-openjdk':
-    ensure  => installed
+    ensure  => $ensure
   }
 
   file {
