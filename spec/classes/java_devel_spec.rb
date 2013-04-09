cd5e1ab8db06b2165a7ff4d3960cf55b309566fb
Justin Lambert <jlambert@eml.cc>
2013-04-08 19:37:11 -0600

    spec update

--- spec/classes/java_devel_spec.rb
+++ spec/classes/java_devel_spec.rb
@@ -5,7 +5,7 @@ describe 'java::devel', :type => :class do
   context "when called with default parameters" do
 
     it { should create_class('java::devel') }
-    it { should create_package('java-1.6.0-openjdk-devel.x86_64') }
+    it { should create_package('java-1.7.0-openjdk-devel.x86_64') }
 
   end
 
